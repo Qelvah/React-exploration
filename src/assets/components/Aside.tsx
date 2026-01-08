b99c95f69278122471d6316b332ac533b52f5712
Qelris <153022149+Qelvah@users.noreply.github.com>
2026-01-08 18:22:39 -0500

    dotdot

--- src/assets/components/Aside.tsx
+++ src/assets/components/Aside.tsx
@@ -3,6 +3,7 @@ import "./Aside.css";
 export default function Aside() {
   return (
     <div id="aside-bar">
+      Theme toggle?
       <ol>
         <li key="1">1</li>
         <li key="1">2</li>
